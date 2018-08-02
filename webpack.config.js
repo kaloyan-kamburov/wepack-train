f071ec228363b15f7046aa13b0ac1440e8df182e
kkamburov <Kaloyan.Kamburov@experian.com>
2018-08-02 17:24:50 +0300

    splitting evironment config files

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,27 @@
-const webpack = require("webpack")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpackMerge = require("webpack-merge");
+const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
 
-
-module.exports = ({ mode }) => {
-	// console.log("---")
-	// console.log(env)
-	return {
-		mode,
-		output: {
-			filename: "bundle.js"
+module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => 
+	webpackMerge(
+		{
+			mode,
+			plugins: [ new HtmlWebpackPlugin(), new webpack.ProgressPlugin() ]
 		},
-		plugins: [
-			new HtmlWebpackPlugin(),
-			new webpack.ProgressPlugin()
-		]
-	}
-}
+		modeConfig(mode)
+	)
+// {
+// 	return {
+// 		mode,
+// 		output: {
+// 			filename: "bundle.js"
+// 		},
+// 		modeConfig(mode),
+// 		load
+// 		plugins: [
+// 			new HtmlWebpackPlugin(),
+// 			new webpack.ProgressPlugin()
+// 		]
+// 	}
+// }
