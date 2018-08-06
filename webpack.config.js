a46c0d53b744cb4e84232a9597fd01d7d6485b5a
kkamburov <Kaloyan.Kamburov@experian.com>
2018-08-06 17:34:06 +0300

    in progress

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,20 @@ const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
 module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => 
 	webpackMerge(
 		{
+			module: {
+				rules: [
+					{
+						test: /\.jpe?g/,
+						use: ["url-loader"]
+					}
+				]
+			},
 			mode,
 			plugins: [ new HtmlWebpackPlugin(), new webpack.ProgressPlugin() ]
 		},
 		modeConfig(mode)
 	)
-// {
+// {	
 // 	return {
 // 		mode,
 // 		output: {
