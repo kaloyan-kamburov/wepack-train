67fa7dc857530622b389350d6a41c558543b54c5
kkamburov <Kaloyan.Kamburov@experian.com>
2018-08-07 13:13:14 +0300

    image

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import makeButton from "./button";
 import { makeColorStyle } from "./button-styles";
 import css from "./footer.css";
 import "./button.css";
+import image from './image.jpg';
 
 const button = makeButton("Yay! A button!");
+console.log(image)
 button.style = makeColorStyle("red");
 document.body.appendChild(button);
 document.body.appendChild(footer);
--- webpack.config.js
+++ webpack.config.js
@@ -4,21 +4,21 @@ const webpackMerge = require("webpack-merge");
 const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
 
 module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => 
-	webpackMerge(
-		{
-			module: {
-				rules: [
-					{
-						test: /\.jpe?g/,
-						use: ["url-loader"]
-					}
-				]
-			},
-			mode,
-			plugins: [ new HtmlWebpackPlugin(), new webpack.ProgressPlugin() ]
+webpackMerge(
+	{
+		module: {
+			rules: [
+				{
+					test: /\.jpe?g/,
+					use: ["url-loader"]
+				}
+			]
 		},
-		modeConfig(mode)
-	)
+		mode,
+		plugins: [ new HtmlWebpackPlugin(), new webpack.ProgressPlugin() ]
+	},
+	modeConfig(mode)
+);
 // {	
 // 	return {
 // 		mode,
