f266a617138c727aa98e80f0db6fde63e18bd2c7
kkamburov <Kaloyan.Kamburov@experian.com>
2018-08-07 13:22:11 +0300

    image

---
+++ src/image.js
@@ -0,0 +1,8 @@
+const makeImage = url => {
+    const image = document.createElement("img");
+    image.src = url;
+    image.height = 200;
+    return image;
+}
+
+module.exports = makeImage;
--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import { makeColorStyle } from "./button-styles";
 import css from "./footer.css";
 import "./button.css";
 import image from './image.jpg';
+import makeImage from './image';
+
+
 
 const button = makeButton("Yay! A button!");
-console.log(image)
 button.style = makeColorStyle("red");
 document.body.appendChild(button);
+
+document.body.appendChild(makeImage(image));
 document.body.appendChild(footer);
 
 
